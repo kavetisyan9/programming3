1b015fe71d778d73b8ee5158a48beabd319a6927
Your Name <you@example.com>
2018-05-23 21:26:09 +0400

    server

--- app.js
+++ app.js
@@ -3,26 +3,25 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-
-var side = 24;
-
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.redirect("index.html");
+    res.redirect("public");
 });
 
 server.listen(3000, function () {
     console.log("Example is running on port 3000");
 });
-var matrix = [];
-function genMatrix(info) {
-    for (var y = 0; y < info[0]; y++) {
+
+
+function genMatrix(w, h) {
+    var matrix = [];
+    for (var y = 0; y < h; y++) {
         matrix[y] = [];
-        for (var x = 0; x < info[1]; x++) {
-            var r = Math.random(100);
+        for (var x = 0; x < w; x++) {
+            var r = random(100);
             if (r < 20) r = 0;
-            else if (r < 60) r = 1;
+            else if (r < 55) r = 1;
             else if (r < 85) r = 2;
             else if (r < 95) r = 3;
             else if (r < 99) r = 4;
@@ -32,6 +31,12 @@ function genMatrix(info) {
     }
     return matrix;
 }
+
+var matrix;
+var w = 30;
+var h = 30;
+var side = 24;
+var grassArr = [], xotakerArr = [], gishatichArr = [], mardArr = [], mardakerArr = [];
 
 var exanak = "garun";
 function poxelExanak() {
@@ -47,184 +52,44 @@ function poxelExanak() {
     else if (exanak == "dsmer") {
         exanak = "garun";
     }
+    io.sockets.emit("send weather", exanak);
 }
 setInterval(poxelExanak, 3000);
 
-function draww() {
-    background("#acacac");
-    fill(0);
-    textSize(24);
-    text("Exanak:" + exanak, 10, 745);
+function setup() {
+    matrix = genMatrix(w, h);
     for (var y in matrix) {
         for (var x in matrix[y]) {
-            if (matrix[y][x] == 0) {
-                fill("#acacac");
-            }
-            else if (matrix[y][x] == 1) {
-                if (exanak == "garun") {
-                    fill("green");
-                }
-                else if (exanak == "amar") {
-                    fill("#00bb00");
-                }
-                else if (exanak == "ashun") {
-                    fill("#799602");
-                }
-                else if (exanak == "dsmer") {
-                    fill("#CEE56E");
-                }
+            if (matrix[y][x] == 1) {
+                grassArr.push(new Grass(x * 1, y * 1, 1));
             }
             else if (matrix[y][x] == 2) {
-                fill("yellow");
+                var ser = (Math.round(Math.random())) / 2;
+                xotakerArr.push(new Xotaker(x * 1, y * 1, 2 + ser, ser));
+                matrix[y][x] += ser;
             }
             else if (matrix[y][x] == 3) {
-                fill("red");
+                var ser = (Math.round(Math.random())) / 2;
+                gishatichArr.push(new Gishatich(x * 1, y * 1, 3 + ser, ser));
+                matrix[y][x] += ser;
             }
             else if (matrix[y][x] == 4) {
-                fill("#fdd17f");
+                var ser = (Math.round(Math.random())) / 2;
+                mardArr.push(new Mard(x * 1, y * 1, 4 + ser, ser));
+                matrix[y][x] += ser;
             }
             else if (matrix[y][x] == 5) {
-                fill("#0099ff");
+                var ser = (Math.round(Math.random())) / 2;
+                mardakerArr.push(new Mardaker(x * 1, y * 1, 5 + ser, ser));
+                matrix[y][x] += ser;
             }
-            rect(x * side, y * side, side, side);
         }
-    }
-    for (var i in arrays[0]) {
-        arrays[0][i].mul();
-    }
-
-    for (var i in arrays[1]) {
-        arrays[1][i].bazmanal();
-        arrays[1][i].utel();
-        arrays[1][i].mahanal();
-    }
-    for (var i in arrays[2]) {
-        arrays[2][i].bazmanal();
-        arrays[2][i].utel();
-        arrays[2][i].mahanal();
-    }
-    for (var i in arrays[3]) {
-        arrays[3][i].bazmanal();
-        arrays[3][i].utelXot();
-        arrays[3][i].utelXotaker();
-        arrays[3][i].mahanal();
-    }
-
-    for (var i in arrays[4]) {
-        if (exanak == "ashun" || exanak == "dsmer") {
-            arrays[4][i].mahanal();
-        }
-        else {
-            arrays[4][i].bazmanal();
-            arrays[4][i].utelGishatich();
-            arrays[4][i].utelMard();
-            arrays[4][i].mahanal();
-        }
-    }
-
-
-    if (arrays[0].length == 900 || (arrays[0].length == 0 && arrays[1].length == 0 && arrays[2].length == 0 && arrays[3].length == 0 && arrays[4].length == 0)) {
-        background("#acacac");
-        textSize(60);
-        fill(0);
-        textAlign(CENTER);
-        text("GAME OVER", 360, 370);
     }
 }
 
 io.on('connection', function (socket) {
-
-    socket.on("send info", genMatrix);
-    io.sockets.emit("send matrix", matrix);
-    socket.on("send arrays", draww);
-    
+    //socket.on("send weather1",poxelExanak);
+    // socket.emit("send weather",exanak);
 
 });
 
-
-
-
-
-
-/*function draww() {
-    background("#acacac");
-    fill(0);
-    textSize(24);
-    text("Exanak:" + exanak, 10, 745);
-    for (var y in matrix) {
-        for (var x in matrix[y]) {
-            if (matrix[y][x] == 0) {
-                fill("#acacac");
-            }
-            else if (matrix[y][x] == 1) {
-                if (exanak == "garun") {
-                    fill("green");
-                }
-                else if (exanak == "amar") {
-                    fill("#00bb00");
-                }
-                else if (exanak == "ashun") {
-                    fill("#799602");
-                }
-                else if (exanak == "dsmer") {
-                    fill("#CEE56E");
-                }
-            }
-            else if (matrix[y][x] == 2) {
-                fill("yellow");
-            }
-            else if (matrix[y][x] == 3) {
-                fill("red");
-            }
-            else if (matrix[y][x] == 4) {
-                fill("#fdd17f");
-            }
-            else if (matrix[y][x] == 5) {
-                fill("#0099ff");
-            }
-            rect(x * side, y * side, side, side);
-        }
-    }
-    for (var i in grassArr) {
-        grassArr[i].mul();
-    }
-
-    for (var i in xotakerArr) {
-        xotakerArr[i].bazmanal();
-        xotakerArr[i].utel();
-        xotakerArr[i].mahanal();
-    }
-    for (var i in gishatichArr) {
-        gishatichArr[i].bazmanal();
-        gishatichArr[i].utel();
-        gishatichArr[i].mahanal();
-    }
-    for (var i in mardArr) {
-        mardArr[i].bazmanal();
-        mardArr[i].utelXot();
-        mardArr[i].utelXotaker();
-        mardArr[i].mahanal();
-    }
-
-    for (var i in mardakerArr) {
-        if (exanak == "ashun" || exanak == "dsmer") {
-            mardakerArr[i].mahanal();
-        }
-        else {
-            mardakerArr[i].bazmanal();
-            mardakerArr[i].utelGishatich();
-            mardakerArr[i].utelMard();
-            mardakerArr[i].mahanal();
-        }
-    }
-
-
-    if (grassArr.length == 900 || (grassArr.length == 0 && xotakerArr.length == 0 && gishatichArr.length == 0 && mardArr.length == 0 && mardakerArr.length == 0)) {
-        background("#acacac");
-        textSize(60);
-        fill(0);
-        textAlign(CENTER);
-        text("GAME OVER", 360, 370);
-    }
-}
-*/
--- public/script.js
+++ public/script.js
@@ -1,94 +1,27 @@
-/*function genMatrix(w, h) {
-    var matrix = [];
-    for (var y = 0; y < h; y++) {
-        matrix[y] = [];
-        for (var x = 0; x < w; x++) {
-            var r = random(100);
-            if (r < 20) r = 0;
-            else if (r < 60) r = 1;
-            else if (r < 85) r = 2;
-            else if (r < 95) r = 3;
-            else if (r < 99) r = 4;
-            else if (r < 100) r = 5;
-            matrix[y][x] = r;
-        }
-    }
-    return matrix;
-}*/
-
-
-var socket = io();
+var socket = io(); 
 
 var w = 30;
 var h = 30;
 var side = 24;
-var grassArr = [], xotakerArr = [], gishatichArr = [], mardArr = [], mardakerArr = [];
-var arrays = [grassArr,xotakerArr,gishatichArr,mardArr,mardakerArr];
-var info = [w, h];
+var exanak = "garun";
+//socket.emit("send weather1", exanak);
 
-socket.emit("send info", info);
+ 
 
-socket.on("send matrix", function(matrix) {
-    for (var y in matrix) {
-        for (var x in matrix[y]) {
-            if (matrix[y][x] == 1) {
-                grassArr.push(new Grass(x * 1, y * 1, 1));
-            }
-            else if (matrix[y][x] == 2) {
-                var ser = (Math.round(Math.random())) / 2;
-                xotakerArr.push(new Xotaker(x * 1, y * 1, 2 + ser, ser));
-                matrix[y][x] += ser;
-            }
-            else if (matrix[y][x] == 3) {
-                var ser = (Math.round(Math.random())) / 2;
-                gishatichArr.push(new Gishatich(x * 1, y * 1, 3 + ser, ser));
-                matrix[y][x] += ser;
-            }
-            else if (matrix[y][x] == 4) {
-                var ser = (Math.round(Math.random())) / 2;
-                mardArr.push(new Mard(x * 1, y * 1, 4 + ser, ser));
-                matrix[y][x] += ser;
-            }
-            else if (matrix[y][x] == 5) {
-                var ser = (Math.round(Math.random())) / 2;
-                mardakerArr.push(new Mardaker(x * 1, y * 1, 5 + ser, ser));
-                matrix[y][x] += ser;
-            }
-        }
-    }
+socket.on("send weather", function(data){
+    console.log(data);
+    exanak = data;
 });
-
-socket.emit("send arrays", arrays); 
-function setup() {
+function setup(){
     createCanvas(side * w, side * (h + 1.5));
     background("#acacac");
     frameRate(5);
-}
-
-//socket.emit("send weather", exanak);
-
-/*function poxelExanak() {
-    if (exanak == "garun") {
-        exanak = "amar";
-    }
-    else if (exanak == "amar") {
-        exanak = "ashun";
-    }
-    else if (exanak == "ashun") {
-        exanak = "dsmer";
-    }
-    else if (exanak == "dsmer") {
-        exanak = "garun";
-    }
-}
-setInterval(poxelExanak, 3000);*/
-
-
-/*function draw() {
-    background("#acacac");
     fill(0);
     textSize(24);
     text("Exanak:" + exanak, 10, 745);
+}   
+
+function draww() {
     for (var y in matrix) {
         for (var x in matrix[y]) {
             if (matrix[y][x] == 0) {
@@ -162,6 +95,6 @@ setInterval(poxelExanak, 3000);*/
         textSize(60);
         fill(0);
         textAlign(CENTER);
-        text("GAME OVER", 360, 370);
+        text("GAME OVER",360 ,370);
     }
-}*/
+}
